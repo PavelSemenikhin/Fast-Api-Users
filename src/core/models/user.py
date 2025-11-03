9e0b548da2edc363b9739814c330f057de25e2ce
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-04 01:15:38 +0200

    Add function for get_user_db

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,12 +1,21 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import Integer, String
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import Integer
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    @classmethod
+    def get_user_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)

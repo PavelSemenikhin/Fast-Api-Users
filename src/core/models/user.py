5aa60b7f1242845fe9674d430ebe2bc9b4c0ddf5
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-04 00:46:30 +0200

    FastApi-Users base User table

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,11 +1,12 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 
 
-class User(Base):
+class User(Base, SQLAlchemyBaseUserTable[int]):
+
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)

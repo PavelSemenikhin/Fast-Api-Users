c35dc4ee07d183106f38c23aa72ae9580aa78434
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-04 22:49:10 +0200

    Create user manager dependency

---
+++ src/api/dependencies/user_manager.py
@@ -0,0 +1,17 @@
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+
+
+from api.dependencies.users import get_user_db
+from core.authentication.user_manager import UserManager
+
+
+if TYPE_CHECKING:
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
+
+async def get_user_manager(
+    user_db: "SQLAlchemyUserDatabase" = Depends(get_user_db),
+):
+    yield UserManager(user_db)

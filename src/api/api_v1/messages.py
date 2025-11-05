5a3292b6a54e1f457979ca18baf40a51223a2de4
Pavlo Semenikhin <pashock1337@gmail.com>
2025-11-05 03:07:57 +0200

    create endpoints for messages (user and superuser)

--- src/api/api_v1/messages.py
+++ src/api/api_v1/messages.py
@@ -1,5 +1,27 @@
 from fastapi import APIRouter, Depends
 
+from api.api_v1.fastapi_users_routers_helper import (
+    current_active_user,
+    current_active_superuser,
+)
 from core.config import settings
+from core.models import User
+from core.schemas.users import UserRead
 
 router = APIRouter(prefix=settings.api.v1.messages, tags=["Messages"])
+
+
+@router.get("/")
+def get_user_messages(user: User = Depends(current_active_user)):
+    return {
+        "messages": ["m1", "m2", "m3"],
+        "User": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets/")
+def get_super_user_messages(user: User = Depends(current_active_superuser)):
+    return {
+        "messages": ["secret-m1", "secret-m2", "secret-m3"],
+        "User": UserRead.model_validate(user),
+    }
